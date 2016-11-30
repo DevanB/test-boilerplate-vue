c92b13029747dee2d9e7407b1e11d1d30e6b0a8e
Devan Beitel <devan.beitel@gmail.com>
2016-11-30 15:16:38 -0600

    Adding back in babel-polyfill and es6-promise

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,7 @@ var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
 require('es6-promise').polyfill()
-// require('babel-polyfill')
+require('babel-polyfill')
 var env = process.env.NODE_ENV
 // check env & config/index.js to decide weither to enable CSS Sourcemaps for the
 // various preprocessor loaders added to vue-loader at the end of this file
@@ -13,7 +13,7 @@ var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: ['babel-polyfill', './src/main.js']
   },
   output: {
     path: config.build.assetsRoot,
